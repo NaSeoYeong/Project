8bc75e1463c7415f11052394174bb7fdb7cc9c7a
NaSeoYeong <syn43018@naver.com>
2023-12-05 20:48:04 +0900

    1205

--- shiny/app.py
+++ shiny/app.py
@@ -8,18 +8,18 @@ from shiny import ui, render, App, reactive
 
 # ui 구현 -----------------------------------------------------------
 app_ui = ui.page_fluid(
-    ui.include_css('style.css'),
+    ui.include_css('./shiny/style.css'),
 
     ui.div(
         {"style": "padding:30px;"},
-        ui.p("현재 날씨 현황", {"style": "font-size: 35px; color: white; font-weight:bolder"}),
+        ui.p("현재 우리 아기는", {"style": "font-size: 35px; color: white; font-weight:bolder"}),
         ui.p(ui.output_ui('get_time'))
     ),
     ui.div(
         ui.layout_column_wrap(
-            ui.value_box('기온', ui.output_text('get_temp'), class_='card border-secondary mb-3'),
+            ui.value_box('온도', ui.output_text('get_temp'), class_='card border-secondary mb-3'),
             ui.value_box('습도', ui.output_text('get_humi'), class_='card border-success mb-3'),
-            ui.value_box('풍속', ui.output_text('get_wind'), class_='card border-light mb-3')
+            ui.value_box('가스', ui.output_text('get_voc'), class_='card border-light mb-3')
         )
     ),
     ui.div(
@@ -31,13 +31,13 @@ app_ui = ui.page_fluid(
 
 
 # 스레드 구현 ----------------------------------------------------------
-_df = pd.read_csv('./data.csv')
-_df['시간'] = pd.to_datetime(_df['시간'])
+_df = pd.read_csv('./shiny/data.csv')
+_df['index'] = pd.to_datetime(_df['index'])
 _now_row = 0
 
-_temp = [0] * 24
-_humi = [0] * 24
-_wind = [0] * 24
+_temp = [0] * 1800
+_humi = [0] * 1800
+_voc = [0] * 1800
 
 _time = ''
 
@@ -45,69 +45,71 @@ _time = ''
 # 서버 구현 ------------------------------------------------------------------
 def server(input, output, session):
 
-    @reactive.poll
+    
     def thread_function():
-        global _df, _now_row, _temp, _humi, _wind, _time
+        global _df, _now_row, _temp, _humi, _voc, _time
 
-        data = _df.iloc[_now_row,:]
+        data = _df.iloc[_now_row:_now_row+60,:]
 
-        _time = str(data['시간'])
+        _time = str(data['index'].iloc[-1])
 
-        if len(_temp) == 24:
+        if len(_temp) == 1800:
             _temp.pop(0)
             _humi.pop(0)
-            _wind.pop(0)
+            _voc.pop(0)
         
-        _temp.append(data['기온'])
-        _humi.append(data['습도'])
-        _wind.append(data['풍속'])
+        _temp.extend(data['temperature'])
+        _humi.extend(data['humidity'])
+        _voc.extend(data['voc'])
 
-        _now_row += 1
+        _now_row += 60
 
-        time.sleep(3)
+        # time.sleep(0.01)
+        
+    reactive.poll(thread_function, 0.01)
 
         
     @output
     @render.text
     def get_temp():
         global _temp
-        reactive.invalidate_later(3)
+        reactive.invalidate_later(0.01)
         return _temp[-1]
     
     @output
     @render.text
     def get_humi():
         global _humi
-        reactive.invalidate_later(3)
+        reactive.invalidate_later(0.01)
         return _humi[-1]
     
     @output
     @render.text
-    def get_wind():
-        global _wind
-        reactive.invalidate_later(3)
-        return _wind[-1]
+    def get_voc():
+        global _voc
+        reactive.invalidate_later(0.01)
+        return _voc[-1]
     
     @output
     @render.ui
     def get_time():
         global _time
-        reactive.invalidate_later(3)
-        return ui.tags.p(f'측정시간: {_time}', {'class':"text-warning"})
+        reactive.invalidate_later(0.01)
+        return ui.tags.p(f'갱신 시간: {_time}', {'class':"text-warning"})
 
     @output
     @render.plot
     def make_plot():
 
-        global _temp, _humi, _wind
-        reactive.invalidate_later(3)
+        global _temp, _humi, _voc
+        reactive.invalidate_later(0.01)
 
         fig, ax = plt.subplots()
         fig.patch.set_alpha(0.0)
 
         ax.plot(_temp, label='temp', color='fuchsia')
         ax.plot(_humi, label='humi', color='lime')
-        ax.plot(_wind, label='wind', color='cyan')
+        ax.plot(_voc, label='voc', color='cyan')
 
         ax.set_facecolor((0, 0, 0, 0))
 
